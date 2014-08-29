48895f191f71bb9213c293b6d64ad547733b854c
mclavel <marcelo.clavel@msn.com>
2014-08-29 03:53:41 -0400

    update

--- js/mqtt_controller.js
+++ js/mqtt_controller.js
@@ -1,29 +1,20 @@
   //Using the HiveMQ public Broker, with a random client Id
  var client = new Messaging.Client("broker.mqttdashboard.com", 8000, "USER_ID_" + parseInt(Math.random() * 100, 10));
+ var msj = 0;
 
  //Gets  called if the websocket/mqtt connection gets disconnected for any reason
  client.onConnectionLost = function (responseObject) {
      //Depending on your scenario you could implement a reconnect logic here
-     alert(responseObject.errorCode);
-     alert("Conexión Perdida: " + responseObject.errorMessage);
+     //alert("Conexión Perdida: " + responseObject.errorMessage);
  };
 
  //Gets called whenever you receive a message for your subscriptions
  client.onMessageArrived = function (message) {
      //Do something with the push message you received
      $('#mensajes').append('<span>Topico: ' + message.destinationName + '  | ' + message.payloadString + '</span><br/>');
- };
-
- //Connect Options
- var options = {
-     timeout: 3,
-     //Gets Called if the connection has sucessfully been established
-     onSuccess: function () {
-         alert("Conectado");
-     },
-     //Gets Called if the connection could not be established
-     onFailure: function (message) {
-         alert("Conexion Fallida failed: " + message.errorMessage);
+     msj = msj+1;
+     if(msj==1000){
+       finish_test();
      }
  };
 
---
+++ js/testing.js
@@ -0,0 +1,59 @@
+var finish_test = function(){
+  client.disconnect();
+  $('#estado').html("");
+  $('#estado').append('<center><h1>########### FIN COMUNICACION ###########</h1></center><br/>');
+}
+
+//Test 1 evalua la cantidad de tráfico generado y la cantidad de procesamiento que se necesita para enviar 1000
+//cadenas de texto, solo enviando el texto (no suscrito) con una calidad de servicio qs.
+var test1 = function (qs) {
+    var run_test_1 = function(qs){
+      for(var i = 0; i < 1000 ; i++){
+        publish("hola mundo "+i,'test1/',qs);
+      }
+      $('#estado').html("");
+      $('#estado').append('<center><h1>########### FIN TEST 1 ###########</h1></center><br/>');
+    }
+    $('#mensajes').html("");
+    $('#estado').append('<center><h1>########### INICIO TEST 1 ###########</h1></center><br/>');
+
+    var options = {
+      timeout: 5,
+      onSuccess: function () {
+        //client.subscribe('test1/#', {qos: 0});
+        run_test_1(qs);
+        },
+      onFailure: function (message) {
+        alert("Error conexion: " + message.errorMessage);
+        }
+      };
+    client.connect(options);
+}
+
+
+//Test 2 evalua la cantidad de tráfico generado y la cantidad de procesamiento que se necesita para enviar 1000
+//cadenas de texto, enviando el texto a un tópico en cuestion y estando suscrito al mismo (con diversas qs).
+var test2 = function (qs_env, qs_rec) {
+    var run_test_2 = function(qs_env){
+      for(var i = 0; i < 1000 ; i++){
+        publish("hola mundo "+i,'test2/',qs_env);
+      }
+      $('#estado').html("");
+      $('#estado').append('<center><h1>########### FIN TEST 2 ###########</h1></center><br/>');
+    }
+    $('#mensajes').html("");
+    $('#estado').html("");
+    $('#estado').append('<center><h1>########### INICIO TEST 2 ###########</h1></center><br/>');
+
+    var options = {
+      timeout: 5,
+      onSuccess: function () {
+        client.subscribe('test2/#', {qos: qs_rec});
+        run_test_2(qs_env);
+        },
+      onFailure: function (message) {
+        alert("Error conexion: " + message.errorMessage);
+        }
+      };
+    client.connect(options);
+}
